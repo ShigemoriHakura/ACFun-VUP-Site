81f5eef9dfc119d75caa61fb320f900c54b3b7d2
ShigemoriHakura <62388797+ShigemoriHakura@users.noreply.github.com>
2020-09-20 18:23:56 +0800

    更新acfun动态转义

--- Frontend/app/template/up/upFeed.tpl.php
+++ Frontend/app/template/up/upFeed.tpl.php
@@ -3,6 +3,19 @@
 <?php include App::$view_root . "/base/sideBar.tpl.php" ?>
 
 <?
+
+function ACFunEscape($str){
+    preg_match_all ("/\[at[^：]+/", $str, $at_array);
+    foreach($at_array[0] as $arr){
+        preg_match_all ("/@[^[]+/", $arr, $uname);
+        preg_match_all ("/uid=[0-9]+/", $arr, $uid);
+        $uname = $uname[0][0];
+        $uid   = str_replace("uid=", "", $uid[0][0]);
+        $str = str_replace($arr, "<a target='_blank' href='https://www.acfun.cn/u/" . $uid . "'>" . $uname . "</a>" , $str);
+    }
+    return $str;
+}
+
 function getMsecToMescdate($msectime)
 {
     $msectime = $msectime * 0.001;
@@ -17,6 +30,7 @@ function getMsecToMescdate($msectime)
     $date = date("Y-m-d H:i:s", $usec);
     return $mescdate = str_replace('x', $sec, $date);
 }
+
 ?>
 
 <div class="page-body">
@@ -60,13 +74,13 @@ function getMsecToMescdate($msectime)
                                                 <? switch($v['resourceType']){
                                                     case 2: //投稿视频
                                                         ?>
-                                                        <h6><a href="https://acfun.cn/u/<?=$v['user']['userId']?>">@<?=$v['user']['userName']?></a>: <span class="pull-right f-14"><?=getMsecToMescdate($v['createTime'])?></span></h6>
+                                                        <h6><a target="_blank" href="https://acfun.cn/u/<?=$v['user']['userId']?>">@<?=$v['user']['userName']?></a>: <span class="pull-right f-14"><?=getMsecToMescdate($v['createTime'])?></span></h6>
                                                         <br>
                                                         <div class="row">
                                                             <div class="col-sm-12">
                                                                 <div class="card">
                                                                     <div class="card-body">
-                                                                        <h6><a href="<?=$v['shareUrl']?>"><?=html_entity_decode($v['caption'])?></a></h6>
+                                                                        <h6><a target="_blank" href="<?=$v['shareUrl']?>"><?=html_entity_decode($v['caption'])?></a></h6>
                                                                         <div class="attachment">
                                                                             <ul class="list-inline">
                                                                                 <li class="list-inline-item"><img class="img-fluid" style="height:200px" src="<?=$v['coverUrl']?>" alt=""></li>
@@ -78,16 +92,16 @@ function getMsecToMescdate($msectime)
                                                         </div>
                                                         <?
                                                         break;
-                                                    case 3: //投稿文章
+                                                    case 3: //投稿文章 已经转义
                                                         ?>
-                                                        <h6><a href="https://acfun.cn/u/<?=$v['user']['userId']?>">@<?=$v['user']['userName']?></a>: <span class="pull-right f-14"><?=getMsecToMescdate($v['createTime'])?></span></h6>
+                                                        <h6><a target="_blank" href="https://acfun.cn/u/<?=$v['user']['userId']?>">@<?=$v['user']['userName']?></a>: <span class="pull-right f-14"><?=getMsecToMescdate($v['createTime'])?></span></h6>
                                                         <br>
                                                         <div class="row">
                                                             <div class="col-sm-12">
                                                                 <div class="card">
                                                                     <div class="card-body">
-                                                                        <h6><a href="<?=$v['shareUrl']?>"><?=html_entity_decode($v['articleTitle'])?></a></h6>
-                                                                        <p><?=html_entity_decode($v['articleBody'])?></p>
+                                                                        <h6><a target="_blank" href="<?=$v['shareUrl']?>"><?=html_entity_decode($v['articleTitle'])?></a></h6>
+                                                                        <p><?=ACFunEscape(html_entity_decode($v['articleBody']))?></p>
                                                                         <div class="attachment">
                                                                             <ul class="list-inline">
                                                                                 <li class="list-inline-item"><img class="img-fluid" style="height:200px" src="<?=$v['coverUrl']?>" alt=""></li>
@@ -99,10 +113,10 @@ function getMsecToMescdate($msectime)
                                                         </div>
                                                         <?
                                                         break;
-                                                    case 10: //普通动态
+                                                    case 10: //普通动态 已经转义
                                                         ?>
-                                                        <h6><a href="https://acfun.cn/u/<?=$v['user']['userId']?>">@<?=$v['user']['userName']?></a>: <span class="pull-right f-14"><?=getMsecToMescdate($v['createTime'])?></span></h6>
-                                                        <p><?=html_entity_decode($v['moment']['text'])?></p>
+                                                        <h6><a target="_blank" href="https://acfun.cn/u/<?=$v['user']['userId']?>">@<?=$v['user']['userName']?></a>: <span class="pull-right f-14"><?=getMsecToMescdate($v['createTime'])?></span></h6>
+                                                        <p><?=ACFunEscape(html_entity_decode($v['moment']['text']))?></p>
                                                         <?if($v['repostSource']){ //转载内容?>
                                                         <br>
                                                         <div class="row">
@@ -113,7 +127,7 @@ function getMsecToMescdate($msectime)
                                                                             case 2:
                                                                             case 3:
                                                                                 ?>
-                                                                                <h6><a href="https://acfun.cn/u/<?=$v['repostSource']['user']['userId']?>">@<?=$v['repostSource']['user']['userName']?></a>:</h6>
+                                                                                <h6><a target="_blank" href="https://acfun.cn/u/<?=$v['repostSource']['user']['userId']?>">@<?=$v['repostSource']['user']['userName']?></a>:</h6>
                                                                                 <div class="attachment">
                                                                                     <ul class="list-inline">
                                                                                         <h6><a href="<?=$v['repostSource']['shareUrl']?>"><?=html_entity_decode($v['repostSource']['caption'])?></a></h6>
@@ -122,10 +136,10 @@ function getMsecToMescdate($msectime)
                                                                                 </div>
                                                                                 <?
                                                                                 break;
-                                                                            case 10: //普通动态
+                                                                            case 10: //普通动态 已经转义
                                                                                 ?>
-                                                                                <h6><a href="https://acfun.cn/u/<?=$v['repostSource']['user']['userId']?>">@<?=$v['repostSource']['user']['userName']?></a>:</h6>
-                                                                                <p><?=html_entity_decode($v['repostSource']['moment']['text'])?></p>
+                                                                                <h6><a target="_blank" href="https://acfun.cn/u/<?=$v['repostSource']['user']['userId']?>">@<?=$v['repostSource']['user']['userName']?></a>:</h6>
+                                                                                <p><?=ACFunEscape(html_entity_decode($v['repostSource']['moment']['text']))?></p>
                                                                                 <? if ($v['repostSource']['moment']['imgs']){?>
                                                                                     <div class="attachment">
                                                                                         <ul class="list-inline">
@@ -139,7 +153,7 @@ function getMsecToMescdate($msectime)
                                                                             break;
                                                                         }?>
                                                                         <br>
-                                                                        <p class="m-0"><a href="<?=$v['repostSource']['shareUrl']?>" target="_blank"><button class="btn btn-square digits btn-secondary"><?=_L('Index_Detail')?></button></a></p>
+                                                                        <p class="m-0"><a target="_blank" href="<?=$v['repostSource']['shareUrl']?>" target="_blank"><button class="btn btn-square digits btn-secondary"><?=_L('Index_Detail')?></button></a></p>
                                                                     </div>
                                                                 </div>
                                                             </div>
@@ -160,7 +174,7 @@ function getMsecToMescdate($msectime)
 
                                                 }?>
                                                 <br>
-                                                <p class="m-0"><a href="<?=$v['shareUrl']?>" target="_blank"><button class="btn btn-square digits btn-secondary"><?=_L('Index_Detail')?></button></a></p>
+                                                <p class="m-0"><a target="_blank" href="<?=$v['shareUrl']?>" target="_blank"><button class="btn btn-square digits btn-secondary"><?=_L('Index_Detail')?></button></a></p>
                                                 <hr>
                                             </div>
                                         </div>
