c592f7aef7292aac31d10102fca98410db662632
ShigemoriHakura <62388797+ShigemoriHakura@users.noreply.github.com>
2020-10-17 00:13:20 +0800

    Revert "Merge pull request #5 from kanoyami/kanoyami"
    This reverts commit 021a38926c39eb36630ee0a17bd5e8e2a596bd34, reversing
    changes made to 30e2327d69dae1d8ab8750d35408b80790194e04.

--- Frontend/app/controller/api/apiAction.php
+++ Frontend/app/controller/api/apiAction.php
@@ -1,19 +1,9 @@
 <?php
-/*
- * @Date: 2020-10-16 20:17:52
- * @LastEditors: kanoyami
- * @LastEditTime: 2020-10-16 23:45:15
- */
 
 namespace app\controller;
-
-function jsonFormat($errcode, $something)
-{
-    return array(
-        "result" => $errcode,
-        "data" => $something
-    );
-}
+use App;
+use biny\lib\Language;
+use Constant;
 
 class apiAction extends baseAction
 {
@@ -35,18 +25,18 @@ class apiAction extends baseAction
             "result" => 1,
             "data" => "Hello world"
         );
-        if ($upid) {
+        if($upid){
             $date = $this->get('day');
             $todayTimestamp = strtotime(date('Y-m-d')) + 86400;
-            if ($date) {
+            if($date){
                 $todayTimestamp = strtotime(date('Y-m-d', $date)) + 86400;
             }
             $rawLatestData = $this->upRawDataDAO->filter([
-                'uperid' => $upid,
-                '<=' => array('up_date' => $todayTimestamp)
-            ])->order(array('up_date' => 'DESC'))->limit(1)->query();
+                'uperid'=>$upid,
+                '<='=>array('up_date'=> $todayTimestamp)
+            ])->order(array('up_date'=>'DESC'))->limit(1)->query();
             $result["data"] = $rawLatestData;
-        } else {
+        }else{
             $result["data"] = "no uperid";
         }
         return json_encode($result);
@@ -58,36 +48,20 @@ class apiAction extends baseAction
             "result" => 1,
             "data" => "Hello world"
         );
-        if ($upid) {
+        if($upid){
             $date = $this->get('day');
             $todayTimestamp = strtotime(date('Y-m-d')) - 1;
-            if ($date) {
+            if($date){
                 $todayTimestamp = strtotime(date('Y-m-d', $date));
             }
             $cronLatestData = $this->upCronDataDAO->filter([
-                'uperid' => $upid,
-                '<=' => array('add_date' => $todayTimestamp)
-            ])->order(array('add_date' => 'DESC'))->limit(1)->query();
+                'uperid'=>$upid,
+                '<='=>array('add_date'=> $todayTimestamp)
+            ])->order(array('add_date'=>'DESC'))->limit(1)->query();
             $result["data"] = $cronLatestData;
-        } else {
+        }else{
             $result["data"] = "no uperid";
         }
         return json_encode($result);
-    }
-
-    public function action_up_fansup_topten_daliy()
-    {
-        $this->response->json(jsonFormat(1, $this->daliyTopService->getTodayFansupTop()));
-    }
-
-    public function action_up_fans_topten()
-    {
-        $this->response->json(jsonFormat(1, $this->statisticsService->fansTopTen()));
-    }
-
-    public function action_up_detial_by_name($upname)
-    {
-         echo($upname);
-        $this->response->json(jsonFormat(1, $this->statisticsService->getUpDetailsByUpName($upname)));
     }
 }
